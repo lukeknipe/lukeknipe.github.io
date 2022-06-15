0153db0e65fbdfa0b431870270428115b2708c5e
Luke Knipe <luke@lukeknipe.com>
2022-06-15 13:46:21 -0700

    Update vote_centers.js

--- vote_centers.js
+++ vote_centers.js
@@ -55,12 +55,11 @@ function initMap() {
     const address = event.feature.getProperty('VC_ADDRESS');
     const room = event.feature.getProperty('ROOM');
     const position = event.feature.getGeometry().get();
-    const sposition = navigator.geolocation.getCurrentPosition(position.coords.latitude);
     const content = `
       <div style="margin-left:20px; margin-bottom:20px;">
         <h2>${name}</h2><p>${address}</p>
         <p><b>Room:</b> ${room}<br/><br/>
-        <p><a href="https://maps.google.com?saddr=${sposition}&daddr=${position.lat()},${position.lng()}">Get directions</a>
+        <p><a href="https://maps.google.com?saddr=${slat}&daddr=${position.lat()},${position.lng()}">Get directions</a>
       </div>
       `;
 
