43f3214647f47f291ee4bd6af8e1e7c93abff07c
Luke Knipe <luke@lukeknipe.com>
2022-06-15 16:01:12 -0700

    Update vote_centers.js

--- vote_centers.js
+++ vote_centers.js
@@ -25,7 +25,7 @@ function initMap() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const pos = {
+          pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
