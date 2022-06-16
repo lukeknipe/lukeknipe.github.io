e9b2026004dca3e42cd4c013ce4e2f564d6ba93b
Luke Knipe <luke@lukeknipe.com>
2022-06-15 23:39:59 -0700

    Update vote_centers.js

--- vote_centers.js
+++ vote_centers.js
@@ -249,7 +249,7 @@ function initMap() {
       <div style="margin-left:10px; margin-bottom:10px;">
         <h2>${name}</h2><p>${address}</p>
         <p><b>Room:</b> ${room}<br/><br/>
-        <p><a href="https://maps.google.com?saddr=${pos.lat},${pos.lng}&daddr=${position.lat()},${position.lng()}">Get directions</a>
+        <p><a href="https://maps.google.com?saddr=${pos.lat},${pos.lng}&daddr=${position.lat()},${position.lng()}" base target="_blank">Get directions</a>
       </div>
       `;
     
