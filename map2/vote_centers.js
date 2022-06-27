2749aef69839189ddae1fa1386ba22c725494b79
Luke Knipe <luke@lukeknipe.com>
2022-06-27 15:17:42 -0700

    Update vote_centers.js

--- map2/vote_centers.js
+++ map2/vote_centers.js
@@ -297,7 +297,7 @@ function initMap() {
   const input = document.createElement('input');
   const options = {
     types: ['address'],
-    componentRestrictions: {country: 'gb'},
+    componentRestrictions: {country: 'us'},
   };
 
   card.setAttribute('id', 'pac-card');
@@ -331,6 +331,106 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       : "Error: Your browser doesn't support geolocation."
   );
   infoWindow.open(map);
+}
+
+async function calculateDistances(data, origin) {
+  const stores = [];
+  const destinations = [];
+
+  // Build parallel arrays for the store IDs and destinations
+  data.forEach((store) => {
+    const storeNum = store.getProperty('storeid');
+    const storeLoc = store.getGeometry().get();
+
+    stores.push(storeNum);
+    destinations.push(storeLoc);
+  });
+
+  // Retrieve the distances of each store from the origin
+  // The returned list will be in the same order as the destinations list
+  const service = new google.maps.DistanceMatrixService();
+  const getDistanceMatrix =
+    (service, parameters) => new Promise((resolve, reject) => {
+      service.getDistanceMatrix(parameters, (response, status) => {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          reject(response);
+        } else {
+          const distances = [];
+          const results = response.rows[0].elements;
+          for (let j = 0; j < results.length; j++) {
+            const element = results[j];
+            const distanceText = element.distance.text;
+            const distanceVal = element.distance.value;
+            const distanceObject = {
+              storeid: stores[j],
+              distanceText: distanceText,
+              distanceVal: distanceVal,
+            };
+            distances.push(distanceObject);
+          }
+
+          resolve(distances);
+        }
+      });
+    });
+
+  const distancesList = await getDistanceMatrix(service, {
+    origins: [origin],
+    destinations: destinations,
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.METRIC,
+  });
+
+  distancesList.sort((first, second) => {
+    return first.distanceVal - second.distanceVal;
+  });
+
+  return distancesList;
+}
+
+function showStoresList(data, stores) {
+  if (stores.length == 0) {
+    console.log('empty stores');
+    return;
+  }
+
+  let panel = document.createElement('div');
+  // If the panel already exists, use it. Else, create it and add to the page.
+  if (document.getElementById('panel')) {
+    panel = document.getElementById('panel');
+    // If panel is already open, close it
+    if (panel.classList.contains('open')) {
+      panel.classList.remove('open');
+    }
+  } else {
+    panel.setAttribute('id', 'panel');
+    const body = document.body;
+    body.insertBefore(panel, body.childNodes[0]);
+  }
+
+
+  // Clear the previous details
+  while (panel.lastChild) {
+    panel.removeChild(panel.lastChild);
+  }
+
+  stores.forEach((store) => {
+    // Add store details with text formatting
+    const name = document.createElement('p');
+    name.classList.add('place');
+    const currentStore = data.getFeatureById(store.storeid);
+    name.textContent = currentStore.getProperty('name');
+    panel.appendChild(name);
+    const distanceText = document.createElement('p');
+    distanceText.classList.add('distanceText');
+    distanceText.textContent = store.distanceText;
+    panel.appendChild(distanceText);
+  });
+
+  // Open the panel
+  panel.classList.add('open');
+
+  return;
 }
 
 window.initMap = initMap;
