60307f284e13dd8063f050af1b54d0d73075dd7b
Luke Knipe <luke@lukeknipe.com>
2022-06-29 21:13:21 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -370,12 +370,12 @@ function showStoresList(data, stores) {
 }
 
 function haversine_distance(origin, storeLoc) {
-      var R = 3958.8; // Radius of the Earth in miles
-      var rlat1 = origin.position.lat() * (Math.PI/180); // Convert degrees to radians
-      var rlat2 = storeLoc.position.lat() * (Math.PI/180); // Convert degrees to radians
-      var difflat = rlat2-rlat1; // Radian difference (latitudes)
-      var difflon = (storeLoc.position.lng()-origin.position.lng()) * (Math.PI/180); // Radian difference (longitudes)
+      const R = 3958.8; // Radius of the Earth in miles
+      const rlat1 = origin.position.lat() * (Math.PI/180); // Convert degrees to radians
+      const rlat2 = storeLoc.position.lat() * (Math.PI/180); // Convert degrees to radians
+      const difflat = rlat2-rlat1; // Radian difference (latitudes)
+      const difflon = (storeLoc.position.lng()-origin.position.lng()) * (Math.PI/180); // Radian difference (longitudes)
 
-      var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+      const d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
       return d;
     }
