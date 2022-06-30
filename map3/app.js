3db3b0a4959ceb85581b2103bc5d001af8c15a78
Luke Knipe <luke@lukeknipe.com>
2022-06-29 22:06:26 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -370,13 +370,11 @@ function showStoresList(data, stores) {
 }
 
 function haversine_distance(origin, storeLoc) {
-      console.log(origin, storeLoc);
       const R = 3958.8; // Radius of the Earth in miles
       const rlat1 = origin.lat() * (Math.PI/180); // Convert degrees to radians
       const rlat2 = storeLoc.lat() * (Math.PI/180); // Convert degrees to radians
       const difflat = rlat2-rlat1; // Radian difference (latitudes)
       const difflon = (storeLoc.lng()-origin.lng()) * (Math.PI/180); // Radian difference (longitudes)
-
       const d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
       return d;
     }
