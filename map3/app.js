ba294d1b58887ceb0b4a9b7e0732463cc1773a66
Luke Knipe <luke@lukeknipe.com>
2022-07-02 17:44:49 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -1,4 +1,19 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
+// Style credit: https://snazzymaps.com/style/1/pale-dawn
 const mapStyle = [{
   'featureType': 'administrative',
   'elementType': 'all',
@@ -248,10 +263,9 @@ async function calculateDistances(data, origin, response) {
     const straightDistance = haversine_distance(origin, storeLoc);
     
     stores.push(storeNum);
-//    destinations.push(storeLoc);
+    destinations.push(storeLoc);
     straightDistances.push(straightDistance);
   });
-  
   
 const topTen = [];
 const nDistance = [];
@@ -272,7 +286,7 @@ stores.forEach(element => {
   
   let result = toptenDistances.map(a => a.destination);
   console.log(result);
-  destinations.push(result);
+//  destinations.push(result);
   
 //  const toptenDistances = straightDistances.sort((a,b) => a-b).slice(0,10);
 //  console.log(toptenDistances);
@@ -388,4 +402,3 @@ function haversine_distance(origin, storeLoc) {
       const d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
       return d;
     }
-
