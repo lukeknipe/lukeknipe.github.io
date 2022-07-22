fa999375c53329634d6597441e442598e4ac7848
Luke Knipe <luke@lukeknipe.com>
2022-07-22 12:58:54 -0700

    Update app.js

--- votecenters/app.js
+++ votecenters/app.js
@@ -445,7 +445,7 @@ function showList(data, stores) {
         const address = document.createElement('p');
         address.classList.add('address');
         const centerAddress = data.getFeatureById(store.storeid);
-        address.textContent = "<a id='foo'>" + centerAddress.getProperty('VC_ADDRESS') + "</a>";
+        address.textContent = centerAddress.getProperty('VC_ADDRESS');
         panel.appendChild(address);
 
         const distanceText = document.createElement('p');
