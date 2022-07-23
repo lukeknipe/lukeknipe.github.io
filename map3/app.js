f2553bb7c959bc5313e51683ab16916cb9659c97
Luke Knipe <luke@lukeknipe.com>
2022-07-23 10:44:58 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -437,6 +437,13 @@ function showList(data, stores) {
     header.href = "http://example.com";
     document.body.appendChild(header);
 
+    var a = document.createElement('a');
+    var linkText = document.createTextNode("my title text");
+    a.appendChild(linkText);
+    a.title = "my title text";
+    a.href = "http://example.com";
+    document.body.appendChild(a);
+
     stores.forEach((store) => {
         // Add store details with text formatting
         const name = document.createElement('p');
