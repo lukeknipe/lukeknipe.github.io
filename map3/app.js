00f8288743c5f536ff1a0accd1ab26c139dd6a78
Luke Knipe <luke@lukeknipe.com>
2022-07-23 23:54:08 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -436,11 +436,10 @@ function showList(data, stores) {
     stores.forEach((store) => {
         // Add store details with text formatting
 
-        var a = document.createElement('a');
+        const a = document.createElement('a');
         a.classList.add('a');
-        var linkText = document.createTextNode("my title text");
+        const linkText = document.createTextNode("my title text");
         a.appendChild(linkText);
-//        a.title = "my title text";
         a.href = "http://example.com";
         panel.appendChild(a);
 
