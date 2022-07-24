814c66e53fd0443984b3c6d315660f9200d68308
Luke Knipe <luke@lukeknipe.com>
2022-07-24 11:30:53 -0700

    Update app.js

--- map3/app.js
+++ map3/app.js
@@ -440,7 +440,7 @@ function showList(data, stores) {
         name.classList.add('place');
         const centerName = data.getFeatureById(store.storeid);
         name.textContent = centerName.getProperty('VC_NAME');
-        p.href = "http://example.com";
+        name.href = "http://example.com";
         panel.appendChild(name);
 
         const address = document.createElement('p');
