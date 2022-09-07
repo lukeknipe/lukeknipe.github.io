b031622b46f6948859679302f7b9e48aa0b9fdd9
Luke Knipe <luke@lukeknipe.com>
2022-09-07 09:38:44 -0700

    Update app.js

--- votecenters/app.js
+++ votecenters/app.js
@@ -462,7 +462,7 @@ function showList(data, stores) {
         const dlng = coordinates.lng();
         const olat = originLocation.lat();
         const olng = originLocation.lng();
-        directionsLink.innerHTML = '<a href="https://maps.google.com?saddr=' + olat +',' + olng + '&daddr=' + dlat + ',' + dlng + '"base target="_blank">Get directions</a>';
+        directionsLink = '<a href="https://maps.google.com?saddr=' + olat +',' + olng + '&daddr=' + dlat + ',' + dlng + '"base target="_blank">Get directions</a>';
         panel.insertAdjacentHTML('beforeend', directionsLink);
     });
 
