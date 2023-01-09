6ecee3cf479bf2fd2578cbeb1829ecf8bbc0d4f5
Luke Knipe <luke@lukeknipe.com>
2023-01-09 13:35:16 -0700

    Update app.js

--- districtfinder/app.js
+++ districtfinder/app.js
@@ -207,10 +207,11 @@ function initMap() {
 
     // Display information in a popup when a marker is clicked.
     map.data.addListener('click', (event) => {
+        const dpos = event.feature.getGeometry().get();
         const id_num = event.feature.getProperty('OBJECTID');
         const content = `
       <div class="popup">
-        <h2>${id_num}</h2>
+        <h2>${id_num}xxx</h2>
       </div>
       `;
         infoWindow.setContent(content);
