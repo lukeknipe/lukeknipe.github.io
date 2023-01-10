abdf7d875214b778681ad1cf362b2f1a9e2914ae
Luke Knipe <luke@lukeknipe.com>
2023-01-09 23:15:30 -0700

    Updates

--- districtfinder/app.js
+++ districtfinder/app.js
@@ -210,6 +210,7 @@ console.log(map.data);
 
     // Display information in a popup when a marker is clicked.
     map.data.addListener('click', (event) => {
+        const id_num = event.feature.getProperty('OBJECTID');
         const content = `
       <div class="popup">
         <h2>${id_num}xxx</h2>
