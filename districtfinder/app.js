87652bfadc2b3ee55517893347e3ea374adda830
Luke Knipe <luke@lukeknipe.com>
2023-01-15 22:55:15 -0700

    Update app.js

--- districtfinder/app.js
+++ districtfinder/app.js
@@ -587,7 +587,7 @@ function initMap() {
 
 function congLite() {
   document.getElementById("cong").style.color = "red";
-	map.overrideStyle = {
+	map.data.overrideStyle = {
 		strokeOpacity: 1
 	};
 }
