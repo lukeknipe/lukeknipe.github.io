511b5423d47e7d0a0b76b2e0be003eb2f247735e
Luke Knipe <luke@lukeknipe.com>
2023-01-18 00:09:46 -0700

    Update app.js

--- districtfinder/app.js
+++ districtfinder/app.js
@@ -586,7 +586,7 @@ function initMap() {
 
 }
 
-function congLite(map) {
+function congLite(initMap) {
 	document.getElementById("cong").style.color = "red";
 	console.log(cong_dist);
 	map.setZoom(13);
