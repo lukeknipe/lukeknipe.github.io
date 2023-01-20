e6061588ed32e55e8f37273dbe3ab0a1c41297b8
Luke Knipe <luke@lukeknipe.com>
2023-01-19 21:37:36 -0700

    Update app.js

--- districtfinder/app.js
+++ districtfinder/app.js
@@ -590,13 +590,15 @@ function initMap() {
 		});
 		infoWindow.open(map);
 		return;
-
-document.getElementById("cong").addEventListener("click", (event) => {
-map.data.forEach(function(feature) {
-if (feature.getProperty("CONG_DIST") == cong_dist) {
-map.data.overrideStyle(feature, {strokeOpacity: 1, strokeWeight: 8});
-}
 });
+
+
+// document.getElementById("cong").addEventListener("click", (event) => {
+// map.data.forEach(function(feature) {
+// if (feature.getProperty("CONG_DIST") == cong_dist) {
+// map.data.overrideStyle(feature, {strokeOpacity: 1, strokeWeight: 8});
+// }
+// });
 
 });
 
